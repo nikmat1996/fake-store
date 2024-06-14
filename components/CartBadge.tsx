37963ddb68c589f9b77d8bed1d1575c49aa07d79
nikmat1996 <nikhilmathew1996@gmail.com>
2024-06-14 14:45:10 +0530

    cart items count in header

---
+++ components/CartBadge.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useCartStore } from '@/lib/store'
+
+const CartBadge = () => {
+  const cart = useCartStore((state) => state.cart);
+
+  if (cart.length === 0) {
+    return null;
+  }
+
+  return (
+    <span className="ml-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+      {cart.length}
+    </span>
+  );
+};
+
+export default CartBadge
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,16 @@
-import Link from 'next/link'
+import Link from "next/link";
+import CartBadge from "./CartBadge";
 
 const Header = () => {
   return (
-    <nav className="space-x-4 mx-auto p-5 ">
-        <Link href={'/'}>Home</Link>
+    <nav className="space-x-4 mx-auto p-5 flex items-center">
+      <Link href={'/'}>Home</Link>
+      <div className="relative flex items-center">
         <Link href={'/cart'}>Cart</Link>
+        <CartBadge />
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Header
